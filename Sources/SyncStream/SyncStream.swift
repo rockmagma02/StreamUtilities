e803922f1684abbc8200ff909c77a8c33114d65a
RuiyangSun <rockmagma02@gmail.com>
2024-04-18 00:01:38 +0800

    feat(SyncStream): build a simple SyncStream

--- Sources/SyncStream/SyncStream.swift
+++ Sources/SyncStream/SyncStream.swift
@@ -12,5 +12,70 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import Dispatch
+import Foundation
+
+/// An synchronous sequence generated from a closure that calls a continuation to produce new elements.
+public struct SyncStream<Element>: Sequence, IteratorProtocol {
+    // MARK: Lifecycle
+
+    /// Constructs an synchronous stream for an element type, using am element-producing closure.
+    ///
+    /// Parameters:
+    ///    - build: A closure that takes a continuation and uses it to produce elements.
+    public init(_: Element.Type = Element.self, _ build: @escaping (SyncStream<Element>.Continuation) -> Void) {
+        let getValueSemaphore = DispatchSemaphore(value: 0)
+        let runFuncitonSemaphore = DispatchSemaphore(value: 0)
+        let continuation = SyncStream<Element>.Continuation(
+            getValueSemaphore: getValueSemaphore,
+            runFuncitonSemaphore: runFuncitonSemaphore
+        )
+
+        self.getValueSemaphore = getValueSemaphore
+        runFunctionSemaphore = runFuncitonSemaphore
+        self.continuation = continuation
+
+        let queue = DispatchQueue(label: "com.SyncStream.\(UUID().uuidString)")
+        queue.async {
+            runFuncitonSemaphore.wait()
+            build(continuation)
+        }
+    }
+
+    /// Constructs an synchronous stream from a given element-producing closure.
+    ///
+    /// Parameters:
+    ///   - produce: A closure that produces elements, returning `nil` when the stream is finished.
+    public init(unfolding produce: @escaping () -> Element?) {
+        self.init(Element.self) { continuation in
+            while let element = produce() {
+                continuation.yield(element)
+            }
+            continuation.finish()
+        }
+    }
+
+    // MARK: Public
+
+    public func next() -> Element? {
+        runFunctionSemaphore.signal()
+        getValueSemaphore.wait()
+        switch continuation.value {
+        case let .element(element):
+            return element
+
+        case .finish:
+            return nil
+        }
+    }
+
+    public func makeIterator() -> SyncStream<Element> {
+        self
+    }
+
+    // MARK: Private
+
+    private let getValueSemaphore: DispatchSemaphore
+    private let runFunctionSemaphore: DispatchSemaphore
+    private let continuation: SyncStream<Element>.Continuation
+}
