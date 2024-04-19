df041287540dfc782165ab2c4ba72b5e0230ec84
RuiyangSun <rockmagma02@gmail.com>
2024-04-19 10:50:31 +0800

    chore(package): add swift-docc-plugin

--- Package.swift
+++ Package.swift
@@ -12,6 +12,9 @@ let package = Package(
             targets: ["SyncStream"]
         ),
     ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-docc-plugin", branch: "main"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
